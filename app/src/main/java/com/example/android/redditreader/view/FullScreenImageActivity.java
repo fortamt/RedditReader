1c9d36a9699afb2bb739a08780832ac1d82d0196
Artem <fortamt@gmail.com>
2023-01-02 19:44:54 +0200

    added fullscreenactivity added land mode added dowloading function

--- app/src/main/java/com/example/android/redditreader/view/FullScreenImageActivity.java
+++ app/src/main/java/com/example/android/redditreader/view/FullScreenImageActivity.java
@@ -6,6 +6,8 @@ import androidx.databinding.DataBindingUtil;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.example.android.redditreader.R;
@@ -19,6 +21,10 @@ public class FullScreenImageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_full_screen_image);
 
         activityFullScreenImageBinding = DataBindingUtil.setContentView(
--- app/src/main/java/com/example/android/redditreader/view/MainActivity.java
+++ app/src/main/java/com/example/android/redditreader/view/MainActivity.java
@@ -7,9 +7,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Toast;
 
 import com.example.android.redditreader.R;
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
     private SwipeRefreshLayout swipeRefreshLayout;
     private MainActivityViewModel mainActivityViewModel;
     private ActivityMainBinding activityMainBinding;
+
+    private static final int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
     private void fillRecycleReview() {
         recyclerView = activityMainBinding.recyclerView;
         adapter = new PostAdapter(childrenList, this);
+        adapter.setStateRestorationPolicy(RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
@@ -65,7 +74,6 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
 
     @Override
     public void onImageClick(int position) {
-
         Intent fullScreenIntent = new Intent(this, FullScreenImageActivity.class);
         fullScreenIntent.setData(Uri.parse(childrenList.get(position).getData().getBetterImageUrl()));
         startActivity(fullScreenIntent);
@@ -73,7 +81,23 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
 
     @Override
     public void onDownloadButtonClick(int position) {
-        Toast.makeText(this, "download", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Downloading started", Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+            }
+        }
+        String url = childrenList.get(position).getData().getBetterImageUrl();
+        downloadImage(url, "downloadedImage");
+    }
 
+    private void downloadImage(String url, String fileName) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("Downloading image");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.allowScanningByMediaScanner();
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
     }
 }
